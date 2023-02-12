9e4f6fa0b9e8470d7d06ff8998fac351ad7b817f
zeddlex1 <a.zeddlex@gmail.com>
2023-02-13 00:58:18 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+str = str.split('')
+    function lengthStrCount() {
+        for (j of bracketsConfig) {
+            for (i = 0; i < str.length - 1; ++i) {
+                if (j[0] == str[i] && j[1] == str[i + 1]) {
+                    str.splice(i, 2)
+                    i = 0
+                }
+            }
+        }
+            return str.length
+    }
+    while (str.length != lengthStrCount()) {
+        lengthStrCount()
+    }
+return str.length > 0 ? false : true
 }
+
+
